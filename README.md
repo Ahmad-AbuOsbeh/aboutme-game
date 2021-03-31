adb2a2eab15d01278e141a13c24a293e56bee0b0
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-03-31 03:25:07 +0300

    added some css formating and 2 questions in js

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 ## Lab02
 
-## About Me guessing game
+## About Me Project.
+
 #### today is Monday 29/3/2021 and I start building my website that can show the users some information about me.
+
+## 29/3/2021 - Monday
+- For JS file :
+ - I asked the user for his name, then I asked him 5 questions about me and he should answer with yes or no.
+
+- For HTML file :
+ - added the main structure of my page.
+ - added an intro about myself.
+ - added a photo of me in the main page.
+ - linked the JS and CSS files with HTML.
+
+- For CSS file :
+ - I reseted the all defualt styling with reset.css file.
+
 
 #### during this week I will add many extra options to make it more professional.
 #### feel free to visit my website, see you there!
@@ -11,6 +26,25 @@
 #### this link I used it for reset.css 
 [reset.css code](https://meyerweb.com/eric/tools/css/reset/)
 
+## Lab03
+#### today was the second day of this project.
+
+## 30/3/2021 - Tuesday
+
+- For JS file:
+ - I asked the users two more questions, the first one to guess a certain number that is in my mind, and he has 4 attempts to answer it.
+ - the second one to guess my favourate sport, and he has 6 attempts.
+ - finally he will see his score of all 7 questions.
+
+- For HTML file :
+ - added top 10 favourate sports for me as an ordered list.
+ - added my experience and education as an unordered list.
+
+- For CSS :
+ - added some formatting of my page.
+
+### feedback questions for the lab :
+
 - How did this go, overall?
   - all things was good,even when I faced some problems and errors, I tried more than one time, then asked TA's for help and all things done correctly.
 
@@ -18,7 +52,7 @@
   - for now I have no questions yet.
 
 - How long did it take you to complete this assignment? And, before you started,
-   - it takes around 2-3 hours
+   - it takes around 3 hours
 
 - how long did you think it would take you to complete this assignment?
-   - I thinked it needs around 1 hour!
+   - I thinked it needs around 1.5 hour!
--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 'use strict';
 
-let myMajor=prompt('Does my major is mechanical engineering ?');
-myMajor=myMajor.toLowerCase();
-switch(myMajor){
+let userName = prompt('what\'s your name ?');
+alert('welcome ' + userName);
+
+let score=0;
+let myMajor = prompt('Does my major is mechanical engineering ?');
+myMajor = myMajor.toLowerCase();
+switch (myMajor) {
 case 'yes':
-case 'y' :
-//   console.log(myMajor);
+case 'y':
+  //   console.log(myMajor);
   alert('Yes you are right');
+  score++;
   break;
 case 'no':
-case 'n' :
-//   console.log(myMajor);
+case 'n':
+  //   console.log(myMajor);
   alert('NO, I am MECHANICAL ENGINEER!');
   break;
 default:
@@ -19,29 +24,31 @@ default:
 }
 
 let myFavouriteone = prompt('Do I love basketball more than the Hicking ?');
-myFavouriteone=myFavouriteone.toLowerCase();
-if (myFavouriteone==='yes'|| myFavouriteone==='y') {
-//   console.log('if1',myFavouriteone);
+myFavouriteone = myFavouriteone.toLowerCase();
+if (myFavouriteone === 'yes' || myFavouriteone === 'y') {
+  //   console.log('if1',myFavouriteone);
   alert('No, I love Hicking More');
 
-}else if (myFavouriteone==='no'||myFavouriteone==='n') {
-//   console.log('if2',myFavouriteone);
+} else if (myFavouriteone === 'no' || myFavouriteone === 'n') {
+  //   console.log('if2',myFavouriteone);
   alert('yes, I love Hicking More');
-}else{
+  score++;
+} else {
   alert('please write yes or no');
 }
 
 let hatefulHabit = prompt('Do I hate the somking ?');
-hatefulHabit=hatefulHabit.toLowerCase();
-switch(hatefulHabit){
+hatefulHabit = hatefulHabit.toLowerCase();
+switch (hatefulHabit) {
 case 'yes':
-case 'y' :
-//   console.log(hatefulHabit);
+case 'y':
+  //   console.log(hatefulHabit);
   alert('Yes you are right');
+  score++;
   break;
 case 'no':
-case 'n' :
-//   console.log(hatefulHabit);
+case 'n':
+  //   console.log(hatefulHabit);
   alert('NO, I hate it!');
   break;
 default:
@@ -49,33 +56,102 @@ default:
   break;
 }
 let whatDoing = prompt('Do I\'m studying the programming now ?');
-whatDoing=whatDoing.toLowerCase();
-if (whatDoing==='yes'|| whatDoing==='y') {
-//   console.log('if1',whatDoing);
+whatDoing = whatDoing.toLowerCase();
+if (whatDoing === 'yes' || whatDoing === 'y') {
+  //   console.log('if1',whatDoing);
   alert('yes sure');
+  score++;
 
-}else if (whatDoing==='no'||whatDoing==='n') {
-//   console.log('if2',whatDoing);
+} else if (whatDoing === 'no' || whatDoing === 'n') {
+  //   console.log('if2',whatDoing);
   alert('no,I\'m studying');
-}else{
+} else {
   alert('please write yes or no');
 }
 
 
-let myFavouritetwo=prompt('Do I love playing football and swimming ?');
+let myFavouritetwo = prompt('Do I love playing football and swimming ?');
 
-switch(myFavouritetwo.toLowerCase()){
+switch (myFavouritetwo.toLowerCase()) {
 case 'yes':
-case 'y' :
-//   console.log(myFavouritetwo);
+case 'y':
+  //   console.log(myFavouritetwo);
   alert('Yes you are right');
+  score++;
   break;
 case 'no':
-case 'n' :
-//   console.log(myFavouritetwo);
+case 'n':
+  //   console.log(myFavouritetwo);
   alert('NO, I love it!');
   break;
 default:
   alert('please write yes or no');
   break;
 }
+
+for (let i = 0; i < 4; i++) {
+
+  let guessNumber = prompt('guess the number that in my mind ? please write number from 1 - 10');
+  guessNumber = Number(guessNumber);
+
+  if (guessNumber === 9) {
+
+    alert('Great! You are right');
+    score++;
+    break;
+  } else if (guessNumber > 9) {
+
+    alert('too high, please try again');
+
+  } else if (guessNumber < 9) {
+
+    alert('too low, please try agian');
+
+  }
+  if (i === 3) {
+
+    alert('you finished the 4 attempts, correct answer is 9');
+    break;
+  }
+}
+// console.log('string');
+let guessSports = ['basketball', 'football', 'cycling', 'hicking', 'swimming', 'badminton'];
+
+for (let j = 0; j < 5; j++) {
+
+  let userAnswer = prompt('what are the best sport for me ?');
+  userAnswer=userAnswer.toLowerCase();
+
+
+
+  for (let i = 0; i < guessSports.length; i++) {
+    // const element = array[i];
+
+    if (userAnswer === guessSports[i]) {
+      break;
+    }
+
+  }
+
+  if (userAnswer === guessSports[1] || userAnswer===guessSports[2]) {
+    alert(`Great! playing ${userAnswer} is the best sport for me`);
+    score++;
+    break;
+  } else if (userAnswer !== guessSports[1] || userAnswer !== guessSports[2]) {
+
+    alert(`NO, its not ${userAnswer} please try again you have ${5 - j} attempts`);
+
+  }
+}
+
+alert(`your score is ${score}/7`);
+// for (let j = 0; j < 5; j++) {
+// const element = array[j];
+// console.log('the first loop', j);
+// for (let i = 0; i < 10; i++) {
+//   console.log('the second loop', i);
+// const element = array[i];
+
+//   }
+
+// }
